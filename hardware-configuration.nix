da4067a717cba0d7c78fe3cb6458c489cd5251a2
mimovnik <jkwidzinski@gmail.com>
2023-07-20 14:46:52 +0200

    Automount MimoDisk

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -22,6 +22,12 @@
     device = "/dev/disk/by-uuid/CCC0-E050";
     fsType = "vfat";
   };
+  
+  fileSystems."/run/media/mimovnik/MimoDisk" = {
+    device = "/dev/disk/by-uuid/288bb4d0-6dde-4a72-ac4e-8b0b18cecbf2";
+    fsType = "ext4";
+    options = [ "nofail" ];
+  };
 
   swapDevices = [ ];
 
