3b2916bdfc520bcdbdbafa86d846b030a733805f
mimovnik <mimovnik@protonmail.com>
2025-01-13 14:53:58 +0100

    Remove keyhint

--- modules/home/desktop/hyprland/hypr/scripts/keyhint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-HYPR_CONFIG=$HOME/.config/hypr/hyprland.conf
-mod_key=$(sed -nre 's/^bind = \$\(.*\), (.*)/\1/p' ${HYPR_CONFIG} | head -n 1)
-
-grep "^bind = " ${HYPR_CONFIG} \
-    | sed "s/-\(-\w\+\)\+//g;s/\$mod/${mod_key}/g;s/Alt/Alt/g;s/bind = //;s/exec //;s/^\s\+//;s/,/ /;s/^\([^ ]\+\) \(.\+\)$/\2: \1/;s/^\s\+//" \
-    | tr -s ' ' \
-    | rofi -dmenu -theme ~/.config/hypr/rofi/keyhint.rasi
