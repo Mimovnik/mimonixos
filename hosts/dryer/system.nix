a6b3b453df2a7674cd885ea813e73a74cd9b0e4c
mimovnik <mimovnik@protonmail.com>
2025-01-23 18:37:05 +0100

    Add dryer host with zfs

---
+++ hosts/dryer/system.nix
@@ -0,0 +1,18 @@
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ./disko-config.nix
+
+    ./zfs.nix
+
+    ../../modules/system/base
+
+    ../../modules/system/common/hostnames.nix
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPb0nrIl2mNjcXMmYWIMalZUGb9Kv/1htsLtqA8hYC/F mimovnik@walle"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdW1UbkbF0p1yTBh2CKv//RsDvot07/t7AtdNGeAsx/ mimo@glados"
+  ];
+}
