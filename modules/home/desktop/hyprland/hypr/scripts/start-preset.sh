b7e9e296851d2466f4b5ed099720abc0d91c4c00
mimovnik <mimovnik@protonmail.com>
2025-02-16 16:10:46 +0100

    Fix moving discord in the preset
    Added more ways to identify windows in presets

--- modules/home/desktop/hyprland/hypr/scripts/start-preset.sh
+++ modules/home/desktop/hyprland/hypr/scripts/start-preset.sh
@@ -6,11 +6,18 @@
 set +e # Disable errexit
 set +u # Disable nounset
 
-function app_is_running() {
-  # Check if there is a window with $CLASS
-  local CLASS="$1"
-  local jq_cmd=".[] | select(.class == \"$CLASS\")"
-  if hyprctl clients -j | jq -e "$jq_cmd" > /dev/null; then
+function is_running() {
+  local WIN_PARAM="$1"
+  local type
+  local value
+  local jq_arg
+
+  type=$(echo "$WIN_PARAM" | cut -d: -f1)
+  value=$(echo "$WIN_PARAM" | cut -d: -f2)
+
+  jq_arg=".[] | select(.$type == \"$value\")"
+
+  if hyprctl clients -j | jq -e "$jq_arg" >/dev/null; then
     return 0
   fi
   return 1
@@ -20,51 +27,61 @@ function wait_for_app() {
   local COUNT=0
   local SLEEP="0.1"
   local LIMIT=100 # NOT SECONDS but number of sleeps
-  local CLASS="$1"
-  echo " - Waiting for $CLASS..."
-  while ! app_is_running "$CLASS"; do
+  local WIN_PARAM="$1"
+  echo " - Waiting for $WIN_PARAM..."
+  while ! is_running "$WIN_PARAM"; do
     sleep "$SLEEP"
     ((COUNT++))
     if [ "$COUNT" -ge "$LIMIT" ]; then
-      echo " - Failed to find $CLASS"
+      echo " - Failed to find $WIN_PARAM"
       break
     fi
   done
 }
 
 function move_app() {
-  local CLASS="$1"
+  local WIN_PARAM="$1"
   local WORKSPACE="$2"
-  echo -n " - Moving $CLASS to $WORKSPACE: "
-  hyprctl dispatch movetoworkspacesilent "$WORKSPACE, class:$CLASS"
+  local type
+  local value
+
+  type=$(echo "$WIN_PARAM" | cut -d: -f1)
+  value=$(echo "$WIN_PARAM" | cut -d: -f2)
+
+  type=$(echo "$type" | tr '[:upper:]' '[:lower:]') # eg. initialtitle instead of initialTitle
+
+  WIN_PARAM="$type:$value"
+
+  echo -n " - Moving $WIN_PARAM to $WORKSPACE: "
+  hyprctl dispatch movetoworkspacesilent "$WORKSPACE, $WIN_PARAM"
 }
 
 function start_app() {
   local APP="$1"
   local WORKSPACE="$2"
-  local CLASS="$3"
+  local WIN_PARAM="$3"
   echo -n " - Starting $APP on workspace $WORKSPACE: "
   hyprctl dispatch exec "[workspace $WORKSPACE silent]" "$APP"
-  wait_for_app "$CLASS"
-  move_app "$CLASS" "$WORKSPACE"
+  wait_for_app "$WIN_PARAM"
+  move_app "$WIN_PARAM" "$WORKSPACE"
 }
 
 function start_singleton_app() {
   local APP="$1"
   local WORKSPACE="$2"
-  local CLASS="$3"
-  if ! app_is_running "$CLASS"; then
-    start_app "$APP" "$WORKSPACE" "$CLASS"
+  local WIN_PARAM="$3" # A parameter that will identify a window https://wiki.hyprland.org/Configuring/Dispatchers/
+  if ! is_running "$WIN_PARAM"; then
+    start_app "$APP" "$WORKSPACE" "$WIN_PARAM"
   else
     echo " - $APP is already running"
   fi
 }
 
 function preset_default() {
-  start_singleton_app "brave --password-store=gnome" 1 "brave-browser"
-  start_singleton_app kitty 2 "kitty"
-  start_singleton_app signal-desktop 5 "signal"
-  start_singleton_app vesktop 7 "vesktop"
+  start_singleton_app "brave --password-store=gnome" 1 "class:brave-browser"
+  start_singleton_app kitty 2 "class:kitty"
+  start_singleton_app signal-desktop 5 "class:signal"
+  start_singleton_app vesktop 7 "initialTitle:Discord"
   hyprctl dispatch forcerendererreload
 }
 
