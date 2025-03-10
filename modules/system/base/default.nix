ace085f772fe1375fd410f219bf63028b02e9709
mimovnik <mimovnik@protonmail.com>
2025-03-10 22:21:00 +0100

    Configure nix gc and remove dirty git warning

--- modules/system/base/default.nix
+++ modules/system/base/default.nix
@@ -70,7 +70,53 @@
   };
 
   # Nix
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix = {
+    settings = {
+      experimental-features = "nix-command flakes";
+
+      max-jobs = "auto";
+
+      # Make legacy nix commands use the XDG base directories instead of creating directories in $HOME.
+      use-xdg-base-directories = true;
+
+      # The maximum number of parallel TCP connections used to fetch files from binary caches and by other downloads.
+      # It defaults to 25. 0 means no limit.
+      http-connections = 128;
+
+      # This option defines the maximum number of substitution jobs that Nix will try to run in
+      # parallel. The default is 16. The minimum value one can choose is 1 and lower values will be
+      # interpreted as 1.
+      max-substitution-jobs = 128;
+
+      # The number of lines of the tail of the log to show if a build fails.
+      log-lines = 25;
+
+      # When free disk space in /nix/store drops below min-free during a build, Nix performs a
+      # garbage-collection until max-free bytes are available or there is no more garbage.
+      # A value of 0 (the default) disables this feature.
+      min-free = 128000000; # 128 MB
+      max-free = 1000000000; # 1 GB
+
+      # Prevent garbage collection from altering nix-shells managed by nix-direnv
+      # https://github.com/nix-community/nix-direnv#installation
+      keep-outputs = true;
+      keep-derivations = true;
+
+      # Automatically detect files in the store that have identical contents, and replaces
+      # them with hard links to a single copy. This saves disk space.
+      auto-optimise-store = true;
+
+      # Whether to warn about dirty Git/Mercurial trees.
+      warn-dirty = false;
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 2d";
+    };
+  };
+
   nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = [
     (final: _prev: {
