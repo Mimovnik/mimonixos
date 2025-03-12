ad5483fb54c760ff51630cb65899eb07d98c3857
mimovnik <mimovnik@protonmail.com>
2025-03-12 07:12:56 +0100

    Add user to dialout group

--- modules/system/base/default.nix
+++ modules/system/base/default.nix
@@ -65,7 +65,7 @@
   users.users.${username} = {
     isNormalUser = true;
     description = username;
-    extraGroups = ["networkmanager" "wheel"];
+    extraGroups = ["networkmanager" "wheel" "dialout"];
     initialHashedPassword = "$y$j9T$LbWJ5akVYZD726dlgAc0c1$cbVuR3O5.Hc7y5r49xIIMzK.T/1evgK0h3Zy719RBgD";
   };
 
