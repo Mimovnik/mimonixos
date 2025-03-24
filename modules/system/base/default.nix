d47c2020257e14ea3aba8f0efc256bfd9f5a702b
mimovnik <mimovnik@protonmail.com>
2025-03-24 17:15:19 +0100

    Clean tmp on boot

--- modules/system/base/default.nix
+++ modules/system/base/default.nix
@@ -13,6 +13,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.tmp.cleanOnBoot = true;
   # Services
   services = {
     printing.enable = true;
