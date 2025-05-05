f658fb7002c33fcc6e486a96a37416c64db55a91
mimovnik <mimovnik@protonmail.com>
2025-05-05 13:06:25 +0200

    Add termi as authorized

--- modules/system/common/authorized-keys.nix
+++ modules/system/common/authorized-keys.nix
@@ -4,5 +4,6 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPb0nrIl2mNjcXMmYWIMalZUGb9Kv/1htsLtqA8hYC/F mimovnik@walle"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBFwwlRsKPurpS0p8fdFst4N5oobJrSGBjLV0iUwp4ez mimovnik@dryer"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEnVkTng/GTwCjsC24ALN09Poarxct0npERMip/XttL9 mimovnik@carbon"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDLxA1utIn20qahXK+GBVXTvSZv7ysDLvT/nn5UfffbB mimovnik@termi"
   ];
 }
