9ddf5bddd17aeb2e48f9374e0672634a60d085a1
mimovnik <mimovnik@protonmail.com>
2025-06-03 19:46:53 +0200

    Add firefox and kdenlive, remove obsidian

--- modules/home/programs/default.nix
+++ modules/home/programs/default.nix
@@ -26,13 +26,14 @@
 
     # gui
     obs-studio
+    firefox
+    kdenlive
     vesktop
     brave
     signal-desktop
     anki
     krita
     onlyoffice-bin
-    obsidian
     nautilus
     gnome-disk-utility
     nextcloud-client
