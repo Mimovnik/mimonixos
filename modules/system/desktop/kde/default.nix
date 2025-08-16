3f36740de79a55c981d40d7e20ce42e9f339b388
mimovnik <mimovnik@protonmail.com>
2025-08-16 18:50:49 +0200

    feat: add finger print auth

--- modules/system/desktop/kde/default.nix
+++ modules/system/desktop/kde/default.nix
@@ -4,4 +4,11 @@
     displayManager.sddm.wayland.enable = true;
     desktopManager.plasma6.enable = true;
   };
+
+  imports = [../../common/fprintd.nix];
+  security.pam.services = {
+    sddm.fprintAuth = true;
+    kscreenlocker.fprintAuth = true;
+    sudo.fprintAuth = true;
+  };
 }
