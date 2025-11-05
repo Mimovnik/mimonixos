eb28a227c7b47d3dde57e1491e3e0e6ef4cf103e
mimovnik <mimovnik@protonmail.com>
2025-11-05 22:51:05 +0100

    feat(libvirt): add virtio filesystem support

--- modules/system/virtualisation/libvirt.nix
+++ modules/system/virtualisation/libvirt.nix
@@ -1,9 +1,15 @@
-{username, ...}: {
+{
+  username,
+  pkgs,
+  ...
+}: {
   programs.virt-manager.enable = true;
 
   users.groups.libvirtd.members = ["${username}"];
 
   virtualisation.libvirtd.enable = true;
+
+  virtualisation.libvirtd.qemu.vhostUserPackages = [pkgs.virtiofsd];
 
   virtualisation.spiceUSBRedirection.enable = true;
 }
