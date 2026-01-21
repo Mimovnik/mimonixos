91ac673ba8e963903366172e539c83191a0c49af
mimovnik <mimovnik@protonmail.com>
2026-01-21 19:02:20 +0100

    chore: change monitor connector to DisplayPort

--- hosts/glados/home.nix
+++ hosts/glados/home.nix
@@ -9,28 +9,29 @@
   ];
 
   # Enable Sway window manager
-  mimo.sway = {
+  mimo.sway = let
+    leftMon = "DP-1";
+    rightMon = "DVI-D-1";
+  in {
     enable = true;
     workspaceOutputs = lib.listToAttrs (
       map (ws: {
         name = toString ws;
-        value = "HDMI-A-2";
+        value = leftMon;
       }) [1 2 3 4 5]
       ++ map (ws: {
         name = toString ws;
-        value = "DVI-D-1";
+        value = rightMon;
       }) [6 7 8 9 10]
     );
 
     # Configure monitor positioning
     outputs = {
-      "HDMI-A-2" = {
-        # Left monitor (2560x1440)
+      "DP-1" = {
         position = "0 0";
         resolution = "2560x1440";
       };
       "DVI-D-1" = {
-        # Right monitor (1920x1080) - positioned after HDMI-A-2's 2560px width
         position = "2560 0";
         resolution = "1920x1080";
       };
