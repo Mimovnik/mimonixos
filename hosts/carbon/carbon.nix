1034034732370f0870e2a0894e510b9551b9325b
mimovnik <mimovnik@protonmail.com>
2026-03-29 20:31:41 +0200

    refactor: don't nest absolute option chain

--- hosts/carbon/carbon.nix
+++ hosts/carbon/carbon.nix
@@ -2,52 +2,50 @@
   inputs,
   self,
   ...
-}: {
-  flake = let
-    hostname = "carbon";
-    system = "x86_64-linux";
-    username = "mimovnik";
-  in {
-    nixosConfigurations.${hostname} = inputs.nixpkgs.lib.nixosSystem {
-      inherit system;
+}: let
+  hostname = "carbon";
+  system = "x86_64-linux";
+  username = "mimovnik";
+in {
+  flake.nixosConfigurations.${hostname} = inputs.nixpkgs.lib.nixosSystem {
+    inherit system;
 
-      specialArgs = {
-        inherit inputs self hostname username;
-      };
+    specialArgs = {
+      inherit inputs self hostname username;
+    };
 
-      modules = [
-        self.nixosModules.systemBase
-        self.nixosModules.systemBoot
-        self.nixosModules.systemDesktopSway
-        self.nixosModules.systemCommonHostnames
-        self.nixosModules.systemSteam
-        self.nixosModules.systemVirtualisationDocker
-        self.nixosModules.systemBinfmt
+    modules = [
+      self.nixosModules.systemBase
+      self.nixosModules.systemBoot
+      self.nixosModules.systemDesktopSway
+      self.nixosModules.systemCommonHostnames
+      self.nixosModules.systemSteam
+      self.nixosModules.systemVirtualisationDocker
+      self.nixosModules.systemBinfmt
 
-        ./_hardware-configuration.nix
-        ./_disko-config.nix
-        ./_zfs.nix
-        ./_nvidia.nix
-        ./_adb-firewall.nix
+      ./_hardware-configuration.nix
+      ./_disko-config.nix
+      ./_zfs.nix
+      ./_nvidia.nix
+      ./_adb-firewall.nix
 
-        inputs.disko.nixosModules.disko
-        inputs.maccel.nixosModules.default
+      inputs.disko.nixosModules.disko
+      inputs.maccel.nixosModules.default
 
-        {
-          hardware.bluetooth = {
-            settings = {
-              General = {
-                ControllerMode = "dual";
-                Experimental = true;
-                FastConnectable = true;
-              };
-              Policy = {
-                AutoEnable = true;
-              };
+      {
+        hardware.bluetooth = {
+          settings = {
+            General = {
+              ControllerMode = "dual";
+              Experimental = true;
+              FastConnectable = true;
+            };
+            Policy = {
+              AutoEnable = true;
             };
           };
-        }
-      ];
-    };
+        };
+      }
+    ];
   };
 }
--- hosts/glados/glados.nix
+++ hosts/glados/glados.nix
@@ -2,44 +2,42 @@
   inputs,
   self,
   ...
-}: {
-  flake = let
-    hostname = "glados";
-    system = "x86_64-linux";
-    username = "mimovnik";
-  in {
-    nixosConfigurations.${hostname} = inputs.nixpkgs.lib.nixosSystem {
-      inherit system;
+}: let
+  hostname = "glados";
+  system = "x86_64-linux";
+  username = "mimovnik";
+in {
+  flake.nixosConfigurations.${hostname} = inputs.nixpkgs.lib.nixosSystem {
+    inherit system;
 
-      specialArgs = {
-        inherit inputs self hostname username;
-      };
-
-      modules = [
-        self.nixosModules.systemBase
-        self.nixosModules.systemBoot
-        self.nixosModules.systemAmdGpu
-        self.nixosModules.systemDesktopSway
-        self.nixosModules.systemCommonHostnames
-        self.nixosModules.systemSteam
-        self.nixosModules.systemVirtualisationLibvirt
-        self.nixosModules.systemVirtualisationBridge
-        self.nixosModules.systemVirtualisationDocker
-        self.nixosModules.systemVirtualisationIncus
-        self.nixosModules.systemCommonOtd
-        self.nixosModules.systemBinfmt
-
-        ./_hardware-configuration.nix
-        ./_disko-config.nix
-        ./_zfs.nix
-
-        inputs.disko.nixosModules.disko
-        inputs.maccel.nixosModules.default
-
-        {
-          powerManagement.cpuFreqGovernor = "performance";
-        }
-      ];
+    specialArgs = {
+      inherit inputs self hostname username;
     };
+
+    modules = [
+      self.nixosModules.systemBase
+      self.nixosModules.systemBoot
+      self.nixosModules.systemAmdGpu
+      self.nixosModules.systemDesktopSway
+      self.nixosModules.systemCommonHostnames
+      self.nixosModules.systemSteam
+      self.nixosModules.systemVirtualisationLibvirt
+      self.nixosModules.systemVirtualisationBridge
+      self.nixosModules.systemVirtualisationDocker
+      self.nixosModules.systemVirtualisationIncus
+      self.nixosModules.systemCommonOtd
+      self.nixosModules.systemBinfmt
+
+      ./_hardware-configuration.nix
+      ./_disko-config.nix
+      ./_zfs.nix
+
+      inputs.disko.nixosModules.disko
+      inputs.maccel.nixosModules.default
+
+      {
+        powerManagement.cpuFreqGovernor = "performance";
+      }
+    ];
   };
 }
