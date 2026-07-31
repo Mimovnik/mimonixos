8d91a073167b17100b0634ba82f72ad9c0fdf87a
mimovnik <mimovnik@protonmail.com>
2026-07-31 13:34:38 +0200

    fix(nixvim): find hidden files

--- modules/home/nixvim/_plugins/telescope.nix
+++ modules/home/nixvim/_plugins/telescope.nix
@@ -1,6 +1,9 @@
 {
   plugins.telescope = {
     enable = true;
+    settings.pickers.find_files = {
+      hidden = true;
+    };
     settings.defaults = {
       # layout_config = {
       #   prompt_position = "top";
