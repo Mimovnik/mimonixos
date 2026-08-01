0796997a1dabab02029f388753056e14f63861b5
mimovnik <mimovnik@protonmail.com>
2026-08-01 13:18:34 +0200

    Show git status on files in yazi

--- modules/home/programs/_yazi.nix
+++ modules/home/programs/_yazi.nix
@@ -1,9 +1,30 @@
-{
+{pkgs, ...}: {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
 
     shellWrapperName = "y";
+
+    plugins.git = {
+      package = pkgs.yaziPlugins.git;
+      setup = true;
+      settings.order = 1500;
+    };
+
+    settings.plugin.prepend_fetchers = [
+      {
+        id = "git";
+        url = "*";
+        run = "git";
+        group = "git";
+      }
+      {
+        id = "git";
+        url = "*/";
+        run = "git";
+        group = "git";
+      }
+    ];
 
     keymap.mgr.prepend_keymap = [
       {
